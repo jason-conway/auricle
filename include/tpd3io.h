3301736e61b8693b19f1f64cd5526017f12f78f1
Jason Conway <jpc@jasonconway.dev>
2021-08-03 20:16:15 -0500

    Enough to get started when hardware arrives

--- include/tpd3io.h
+++ include/tpd3io.h
@@ -15,63 +15,85 @@
 #include "auricle.h"
 #include "imxrt.h"
 
-#define D3SR_POW_ON 1
-#define D3SR_POW_OFF 0
+// Outputs on GPIO Register 6
+#define GPIO_DR_SIG_OUT (IMXRT_GPIO6.DR) // GPIO6 Data Register
+#define GPIO_PSR_SIG_OUT (IMXRT_GPIO6.PSR) // GPIO6 Pad Sample Register
 
-#define GPIO_REG_SIG_OUT (IMXRT_GPIO6.DR)
+// Inputs on Data Register 9 
+#define GPIO_DR_SIG_IN (IMXRT_GPIO9.DR) // GPIO9 Data Register
+#define GPIO_PSR_SIG_IN (IMXRT_GPIO9.PSR) // GPIO9 Pad Sample Register
 
-#define GPIO_IOMUX_SIG_POW IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_03
-#define GPIO_PAD_SIG_POW IOMUXC_SW_PAD_CTL_PAD_GPIO_AD_B0_03
-#define GPIO_MASK_SIG_POW ( 1<< 3)
+// SW_MUX_CTL Registers
+#define GPIO_IOMUX_SIG_POW IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_03 // pg 476
+#define GPIO_IOMUX_SIG_SEL IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_02 // pg 475
+#define GPIO_IOMUX_SIG_USB IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_04   // pg 432
+#define GPIO_IOMUX_SIG_OPT IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_05   // pg 433
+#define GPIO_IOMUX_SIG_RCA IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_06   // pg 435
+#define GPIO_IOMUX_SIG_BNC IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_08   // pg 437
 
-#define GPIO_IOMUX_SIG_SEL IOMUXC_SW_MUX_CTL_PAD_GPIO_AD_B0_02
-#define GPIO_PAD_SIG_SEL IOMUXC_SW_PAD_CTL_PAD_GPIO_AD_B0_02
-#define GPIO_MASK_SIG_SEL (1 << 2)
+// SW_PAD_CTL Registers
+#define GPIO_PAD_SIG_POW IOMUXC_SW_PAD_CTL_PAD_GPIO_AD_B0_03 // pg 636
+#define GPIO_PAD_SIG_SEL IOMUXC_SW_PAD_CTL_PAD_GPIO_AD_B0_02 // pg 634
+#define GPIO_PAD_SIG_USB IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_04   // pg 566
+#define GPIO_PAD_SIG_OPT IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_05   // pg 568
+#define GPIO_PAD_SIG_RCA IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_06   // pg 569
+#define GPIO_PAD_SIG_BNC IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_08   // pg 573
 
-#define GPIO_REG_SIG_IN (IMXRT_GPIO9.DR)
+// Bitmasks
+#define GPIO_MASK_SIG_POW (1 << 3) // 8
+#define GPIO_MASK_SIG_SEL (1 << 2) // 4
+#define GPIO_MASK_SIG_USB (1 << 4) // 16
+#define GPIO_MASK_SIG_OPT (1 << 5) // 32
+#define GPIO_MASK_SIG_RCA (1 << 6) // 64
+#define GPIO_MASK_SIG_BNC (1 << 8) // 256
 
-#define GPIO_IOMUX_SIG_USB IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_04
-#define GPIO_PAD_SIG_USB IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_04
-#define GPIO_MASK_SIG_USB (1 << 4) 
+// MUX_MODE ALT5 ==> GPIO
+#define GPIO_MUX_MODE_ALT5 (0b0101 | 0b00010000)
 
-#define GPIO_IOMUX_SIG_OPT IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_05
-#define GPIO_PAD_SIG_OPT IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_05
-#define GPIO_MASK_SIG_OPT (1 << 5) 
-
-#define GPIO_IOMUX_SIG_RCA IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_06
-#define GPIO_PAD_SIG_RCA IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_06
-#define GPIO_MASK_SIG_RCA (1 << 6)
-
-#define GPIO_IOMUX_SIG_BNC IOMUXC_SW_MUX_CTL_PAD_GPIO_EMC_08
-#define GPIO_PAD_SIG_BNC IOMUXC_SW_PAD_CTL_PAD_GPIO_EMC_08
-#define GPIO_MASK_SIG_BNC (1 << 8)
-
-#define GPIO_MUX 0b10101
+#define D3_PLL_NOT_LOCKED 0
+#define D3_PLL_LOCKED 1
+#define D3_INCORRECT_MODE 2
 
 class TPD3IO
 {
 public:
     TPD3IO(void);
 
-
-    
-    
 private:
-    enum D3_IO
+    enum D3InputMode
     {
-        SIG_POW,
-        SIG_SEL,
-        SIG_USB,
-        SIG_OPT,
-        SIG_RCA,
-        SIG_BNC
+        MODE_NULL,
+        USB,
+        OPT,
+        RCA,
+        BNC
     };
 
-    void init(void);
-    void powerOn(void);
-    void switchInput(void);
-    
-};
+    enum D3Power
+    {
+        POWER_OFF,
+        POWER_ON
+    };
 
+    typedef struct D3Status
+    {
+        uint8_t power;
+        volatile uint8_t mode;
+    } D3Status;
+    
+    D3Status d3status;
+
+    void init(void);
+    void togglePower(void);
+    void switchInput(void);
+    uint8_t readGPIO(void);
+    uint8_t pllStatus(void);
+    uint8_t checkPower(void);
+    uint8_t checkSigUSB(void);
+    uint8_t checkSigOPT(void);
+    uint8_t checkSigRCA(void);
+    uint8_t checkSigBNC(void);
+
+};
 
 #endif
--- src/tpd3io.cpp
+++ src/tpd3io.cpp
@@ -11,24 +11,28 @@
 
 #include "tpd3io.h"
 
+/**
+ * @brief Construct a new TPD3IO::TPD3IO object
+ * 
+ */
 TPD3IO::TPD3IO(void)
 {
     init();
 }
 
 /**
- * @brief 
+ * @brief Configure GPIO for interfacing with the D3
  * 
  */
 void TPD3IO::init(void)
 {
     // Outputs on GPIO6, inputs on GPIO9
-    volatile uint32_t *gpio_pow_reg = &GPIO_REG_SIG_OUT;
-    volatile uint32_t *gpio_sel_reg = &GPIO_REG_SIG_OUT;
-    volatile uint32_t *gpio_usb_reg = &GPIO_REG_SIG_IN;
-    volatile uint32_t *gpio_opt_reg = &GPIO_REG_SIG_IN;
-    volatile uint32_t *gpio_rca_reg = &GPIO_REG_SIG_IN;
-    volatile uint32_t *gpio_bnc_reg = &GPIO_REG_SIG_IN;
+    volatile uint32_t *gpio_pow_reg = &GPIO_DR_SIG_OUT;
+    volatile uint32_t *gpio_sel_reg = &GPIO_DR_SIG_OUT;
+    volatile uint32_t *gpio_usb_reg = &GPIO_DR_SIG_IN;
+    volatile uint32_t *gpio_opt_reg = &GPIO_DR_SIG_IN;
+    volatile uint32_t *gpio_rca_reg = &GPIO_DR_SIG_IN;
+    volatile uint32_t *gpio_bnc_reg = &GPIO_DR_SIG_IN;
 
     volatile uint32_t *gpio_pow_mux = &GPIO_IOMUX_SIG_POW;
     volatile uint32_t *gpio_sel_mux = &GPIO_IOMUX_SIG_SEL;
@@ -46,40 +50,137 @@ void TPD3IO::init(void)
 
     *(gpio_pow_reg + 1) |= GPIO_MASK_SIG_POW;
     *(gpio_pow_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_pow_mux) = GPIO_MUX;
+    *(gpio_pow_mux) = GPIO_MUX_MODE_ALT5;
 
     *(gpio_sel_reg + 1) |= GPIO_MASK_SIG_SEL;
     *(gpio_sel_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_sel_mux) = GPIO_MUX;
+    *(gpio_sel_mux) = GPIO_MUX_MODE_ALT5;
 
     *(gpio_usb_reg + 1) &= ~GPIO_MASK_SIG_USB;
     *(gpio_usb_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_usb_mux) = GPIO_MUX;
+    *(gpio_usb_mux) = GPIO_MUX_MODE_ALT5;
 
     *(gpio_opt_reg + 1) &= ~GPIO_MASK_SIG_OPT;
     *(gpio_opt_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_opt_mux) = GPIO_MUX;
+    *(gpio_opt_mux) = GPIO_MUX_MODE_ALT5;
 
     *(gpio_rca_reg + 1) &= ~GPIO_MASK_SIG_RCA;
     *(gpio_rca_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_rca_mux) = GPIO_MUX;
+    *(gpio_rca_mux) = GPIO_MUX_MODE_ALT5;
 
     *(gpio_bnc_reg + 1) &= ~GPIO_MASK_SIG_BNC;
     *(gpio_bnc_pad) = IOMUXC_PAD_DSE(7);
-    *(gpio_bnc_mux) = GPIO_MUX;
+    *(gpio_bnc_mux) = GPIO_MUX_MODE_ALT5;
+
+    d3status.mode = MODE_NULL;
+    d3status.power = POWER_OFF;
 
 }
 
-void TPD3IO::powerOn(void)
+/**
+ * @brief Write SIG_POW HIGH for 50ms to toggle power on the D3 board
+ * 
+ */
+void TPD3IO::togglePower(void)
 {
     GPIO6_DR_SET = GPIO_MASK_SIG_POW;
-    delayMicroseconds(1250);
+    delayMicroseconds(50000);
     GPIO6_DR_CLEAR = GPIO_MASK_SIG_POW;
+    d3status.power = (d3status.power) ? POWER_OFF : POWER_ON;
 }
 
+/**
+ * @brief Write SIG_SEL HIGH for 50ms switch inputs on the D3 board
+ * 
+ */
 void TPD3IO::switchInput(void)
 {
     GPIO6_DR_SET = GPIO_MASK_SIG_SEL;
-    delayMicroseconds(1250);
+    delayMicroseconds(50000);
     GPIO6_DR_CLEAR = GPIO_MASK_SIG_SEL;
+}
+
+/**
+ * @brief Check if SIG_USB is HIGH
+ * 
+ * @return uint8_t 
+ */
+uint8_t TPD3IO::checkSigUSB(void)
+{
+    if (GPIO_PSR_SIG_IN & GPIO_MASK_SIG_USB)
+    {
+        d3status.mode = USB;
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Check if SIG_OPT is HIGH
+ * 
+ * @return uint8_t 
+ */
+uint8_t TPD3IO::checkSigOPT(void)
+{
+    if (GPIO_PSR_SIG_IN & GPIO_MASK_SIG_OPT)
+    {
+        d3status.mode = OPT;
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Check if SIG_RCA is HIGH
+ * 
+ * @return uint8_t 
+ */
+uint8_t TPD3IO::checkSigRCA(void)
+{
+    if (GPIO_PSR_SIG_IN & GPIO_MASK_SIG_RCA)
+    {
+        d3status.mode = RCA;
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Check if SIG_BNC is HIGH
+ * 
+ * @return uint8_t 
+ */
+uint8_t TPD3IO::checkSigBNC(void)
+{
+    if (GPIO_PSR_SIG_IN & GPIO_MASK_SIG_BNC)
+    {
+        d3status.mode = BNC;
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * @brief Check if the D3's PLL has locked in
+ * 
+ * @return uint8_t 
+ */
+uint8_t TPD3IO::pllStatus(void)
+{
+    if (d3status.mode == OPT)
+    {
+        for (size_t i = 0; i < 8; i++) // 2 seconds
+        {
+            uint32_t startingCycleCount = ARM_DWT_CYCCNT;
+
+            if (!(checkSigOPT)) // SIG_OPT remains HIGH >= 2 seconds
+            {
+                return D3_PLL_NOT_LOCKED;
+            }
+            while (ARM_DWT_CYCCNT - startingCycleCount < (uint32_t)99000000); // 250ms
+
+        } 
+        return D3_PLL_LOCKED;
+    }
+    return D3_INCORRECT_MODE;
 }
