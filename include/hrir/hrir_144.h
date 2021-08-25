d59ca33231ae8b2d3d957b3b81747e6178cb6fbd
Jason Conway <jpc@jasonconway.dev>
2021-08-24 20:59:02 -0500

    Added packaged HRIR arrays

---
+++ include/hrir/hrir_144.h
@@ -0,0 +1,18 @@
+/**
+ * @file hrir_144.h
+ * @author Jason Conway (jpc@jasonconway.dev)
+ * @brief HRIR coefficients for 144 degrees azimuth
+ * @version 0.1
+ * @date 2021-08-24
+ * 
+ * @copyright Copyright (c) 2021 Jason Conway. All rights reserved.
+ */
+
+#pragma once
+
+#include "auricle.h"
+#include "convolvIR.h"
+#include "stdint.h"
+
+float32_t __attribute__((section(".flashmem"))) HRIR_144[2][8192] = {
+	{
