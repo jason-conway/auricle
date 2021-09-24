a6046911b77e571ddfb5f56877914f32058b8017
Jason Conway <jpc@jasonconway.dev>
2021-09-23 21:15:54 -0500

    Removed some C style casts, null -> nullptr

--- src/ash.cpp
+++ src/ash.cpp
@@ -21,28 +21,28 @@ ASH::ASH(void)
 
 void ASH::init(void)
 {
-	subshell.newCmd("hostname", "", this->hostname, NULL); // Set hostname / prompt
-	subshell.newCmd("unknown", "", this->unknownCommand, NULL); 
-	subshell.newCmd("help", "Show help for the specified command", this->showHelp, NULL); 
+	subshell.newCmd("hostname", "", this->hostname, nullptr); // Set hostname / prompt
+	subshell.newCmd("unknown", "", this->unknownCommand, nullptr); 
+	subshell.newCmd("help", "Show help for the specified command", this->showHelp, nullptr); 
 
-	subshell.newCmd("togglepower", "Toggle power on the D3", this->togglePower, NULL);
-	subshell.newCmd("togglemode", "Toggle D3 input mode", this->switchInput, NULL);
-	subshell.newCmd("pttoggle", "Toggle audio passthrough", this->audioPassthrough, NULL);
-	subshell.newCmd("status", "Get status of the D3", this->currentStatus, NULL);
-	subshell.newCmd("sangle", "Set HRIR angle", this->setAngle, NULL);
-	subshell.newCmd("audiomemory", "View current and maximum audio memory", this->audioMemory, NULL);
-	subshell.newCmd("reboot", "Reboot Auricle", this->reboot, NULL);
-	subshell.newCmd("clear", "Clear screen", this->clear, NULL);
-	subshell.newCmd("memuse", "View amount of RAM free", this->memoryUse, NULL);
-	subshell.newCmd("lscmd", "List all commands", this->listCommands, NULL);
+	subshell.newCmd("togglepower", "Toggle power on the D3", this->togglePower, nullptr);
+	subshell.newCmd("togglemode", "Toggle D3 input mode", this->switchInput, nullptr);
+	subshell.newCmd("pttoggle", "Toggle audio passthrough", this->audioPassthrough, nullptr);
+	subshell.newCmd("status", "Get status of the D3", this->currentStatus, nullptr);
+	subshell.newCmd("sangle", "Set HRIR angle", this->setAngle, nullptr);
+	subshell.newCmd("audiomemory", "View current and maximum audio memory", this->audioMemory, nullptr);
+	subshell.newCmd("reboot", "Reboot Auricle", this->reboot, nullptr);
+	subshell.newCmd("clear", "Clear screen", this->clear, nullptr);
+	subshell.newCmd("memuse", "View amount of RAM free", this->memoryUse, nullptr);
+	subshell.newCmd("lscmd", "List all commands", this->listCommands, nullptr);
 
 	if (subshell.status < 0)
 	{
-		if (subshell.status == subshell.SUBSHELL_REALLOC_FAILURE)
+		if (subshell.status == SUBSHELL_REALLOC_FAILURE)
 		{
 			SerialUSB.printf(flashmem("Error: realloc"));
 		}
-		if (subshell.status == subshell.SUBSHELL_SNPRINTF_FAILURE)
+		if (subshell.status == SUBSHELL_SNPRINTF_FAILURE)
 		{
 			SerialUSB.printf(flashmem("Error: snprintf"));
 		}
@@ -65,9 +65,10 @@ void ASH::togglePower(void *)
 
 void ASH::setAngle(void *)
 {
-	if (char *arg = subshell.getArg())
+	char *cmdArg = nullptr;
+	if (subshell.getArg(&cmdArg))
 	{
-		uint16_t angle = static_cast<uint16_t>(atoi(arg));
+		uint16_t angle = static_cast<uint16_t>(atoi(cmdArg));
 		SerialUSB.printf("Setting angle: %d degrees\r\n", angle);
 		convolvIR.convertIR(__builtin_round(static_cast<float32_t>(angle) / 3.6));
 		SerialUSB.printf("Done\r\n");
@@ -113,8 +114,7 @@ void ASH::memoryUse(void *)
 {
 	extern unsigned long _heap_end;
 	extern char *__brkval;
-
-	SerialUSB.printf("Memory free: %8d\r\n", ((char *)&_heap_end - __brkval));
+	SerialUSB.printf("Memory free: %8d\r\n", (reinterpret_cast<char *>(&_heap_end) - __brkval));
 }
 
 void ASH::reboot(void *)
@@ -122,19 +122,19 @@ void ASH::reboot(void *)
 	usb_serial_flush_output();
 	usb_serial_flush_input();
 	SerialUSB.printf("Auricle Rebooting\r\n");
-	(*(volatile uint32_t *)0xE000ED0C) = 0x05FA0004;
+	*reinterpret_cast<volatile uint32_t *>(0xE000ED0C) = 0x05FA0004;
 }
 
 void ASH::unknownCommand(void *)
 {
-	ASH::hostname(NULL);
+	ASH::hostname(nullptr);
 	SerialUSB.printf("Unknown command\r\n");
 	SerialUSB.flush();
 }
 
 void ASH::clear(void *)
 {
-	static char clrSeq[] = {0x1B, 0x5B, 0x32, 0x4A, 0x00};
+	const char clrSeq[] = {0x1B, 0x5B, 0x32, 0x4A, 0x00};
 	SerialUSB.printf("%s", clrSeq);
 }
 
@@ -146,9 +146,9 @@ void ASH::hostname(void *)
 
 void ASH::motd(void)
 {
-	this->clear(NULL);
+	this->clear(nullptr);
 	SerialUSB.printf("Auricle Shell\r\n");
 	SerialUSB.printf("Version 0.1\r\n");
 	SerialUSB.printf("Copyright (c) 2021 Jason Conway\r\n\r\n");
-	this->hostname(NULL);
+	this->hostname(nullptr);
 }
