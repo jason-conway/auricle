4d1282bd34f3271b643482bf94cc6577ca73eb00
Jason Conway <jpc@jasonconway.dev>
2021-10-17 13:33:10 -0500

    Inlined defines

--- src/usbDescriptor.c
+++ src/usbDescriptor.c
@@ -11,22 +11,5 @@
 
 #include "usb_names.h"
 
-#define AURICLE_MANUFACTURER_NAME {'J', 'A', 'S', 'O', 'N', ' ', 'C', 'O', 'N', 'W', 'A', 'Y'}
-#define AURICLE_MANUFACTURER_NAME_LEN 12
-
-#define AURICLE_PRODUCT_NAME {'a', 'u', 'r', 'i', 'c', 'l', 'e'}
-#define AURICLE_PRODUCT_NAME_LEN 7
-
-struct usb_string_descriptor_struct usb_string_product_name =
-	{
-		2 + AURICLE_PRODUCT_NAME_LEN * 2,
-		3,
-		AURICLE_PRODUCT_NAME
-	};
-
-struct usb_string_descriptor_struct usb_string_manufacturer_name =
-	{
-		2 + AURICLE_MANUFACTURER_NAME_LEN * 2,
-		3,
-		AURICLE_MANUFACTURER_NAME
-	};
+struct usb_string_descriptor_struct usb_string_product_name = {16, 3, {'a', 'u', 'r', 'i', 'c', 'l', 'e'}};
+struct usb_string_descriptor_struct usb_string_manufacturer_name = {26, 3, {'J', 'A', 'S', 'O', 'N', ' ', 'C', 'O', 'N', 'W', 'A', 'Y'}};
